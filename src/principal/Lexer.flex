d0b20306e17c83da6e865a5f6e65ae975a173527
Gustavo <elmergustavo79@gmail.com>
2020-10-13 18:53:07 -0600

    Primer commit Proyecto Compiladores

---
+++ src/principal/Lexer.flex
@@ -0,0 +1,32 @@
+package principal;
+import static principal.Tokens.*;
+%%
+%class Lexer
+%type Tokens
+L=[a-zA-Z_]+
+D=[0-9]+
+espacio=[ ,\t,\r,\n]+
+%{
+    public String lexeme;
+%}
+%%
+entero |
+cadena |
+real |
+boleano |
+si |
+sino |
+escribir |
+leer |
+desde {lexeme=yytext(); return Reservadas;}
+{espacio} {/*Ignore*/}
+"//".* {/*Ignore*/}
+"=" {return Igual;}
+"+" {return Suma;}
+"-" {return Resta;}
+"*" {return Multiplicacion;}
+"/" {return Division;}
+"%" {return Modulo;}
+{L}({L}|{D})* {lexeme=yytext(); return Identificador;}
+("(-"{D}+")")|{D}+ {lexeme=yytext(); return Numero;}
+ . {return ERROR;}
---
+++ src/principal/Principal_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+
+import java.io.File;
+
+/**
+ *
+ * @author Charly Ponce
+ */
+public class Principal_1 {
+    public static void main(String[] args) {
+        
+        String ruta = "C:/Users/elmer/Desktop/ProyectoCompiladores/src/principal/Lexer.flex";
+        generarLexer(ruta);
+    }
+    public static void generarLexer(String ruta){
+        File archivo = new File(ruta);
+        JFlex.Main.generate(archivo);
+    }
+}
